b1d4cd2ad8d93777752d506116b243a4ab03248b
Aditya Kotari <aditya.kotari@gmail.com>
2021-05-28 12:39:07 +0530

    home page + prototype solo page

--- frontend/src/components/screens/Home.js
+++ frontend/src/components/screens/Home.js
@@ -1,17 +1,54 @@
-import React, {Component, useState, useEffect, useContext} from 'react';
+import React, { Component, useState, useEffect, useContext } from "react"
+import { useHistory } from "react-router"
+import { Button, Grid, GridColumn } from "semantic-ui-react"
 
-import Topnav from '../Topnav.js'; 
+import Topnav from "../Topnav.js"
 
 const Home = () => {
-
-   return (
-
-    <div>
-      <Topnav />
-      <h1 style={{fontSize: "200px"}}>Poggers</h1>
-    </div>
-
-  )
+    let history = useHistory()
+    return (
+        <div>
+            <Topnav />
+            <Grid className="homeB" columns={3} relaxed="very" stackable>
+                <Grid.Column verticalAlign="middle">
+                    <Button
+                        circular
+                        size="massive"
+                        className="homebuttons"
+                        onClick={() => {
+                            history.push("/solo")
+                        }}
+                    >
+                        Exercise Solo
+                    </Button>
+                </Grid.Column>
+                <Grid.Column verticalAlign="middle">
+                    <Button
+                        circular
+                        size="massive"
+                        className="homebuttons"
+                        onClick={() => {
+                            history.push("/random")
+                        }}
+                    >
+                        Random Match
+                    </Button>
+                </Grid.Column>
+                <Grid.Column verticalAlign="middle">
+                    <Button
+                        circular
+                        size="massive"
+                        className="homebuttons"
+                        onClick={() => {
+                            history.push("/friendplay")
+                        }}
+                    >
+                        Play with a friend
+                    </Button>
+                </Grid.Column>
+            </Grid>
+        </div>
+    )
 }
 
-export default Home; 
+export default Home
