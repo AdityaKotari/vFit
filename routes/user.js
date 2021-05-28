72c4e4f83136c0cb1d690bc32cc11a62a23ccc7d
Pranav Balaji <73339142+greenfish8090@users.noreply.github.com>
2021-05-28 17:40:42 +0530

    Connected db and did signup and login

--- routes/user.js
+++ routes/user.js
@@ -6,33 +6,72 @@ var router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const requireLogin = require('../utility/requireLogin.js')
+const con = require('../utility/dbConnection.js')
+const jwt_expiry_time = "3 days"
 
-
-//adds a new user to db, requires {name, email, password}
+//adds a new user to db, requires { username, password, email, city_id }
 router.post('/signup', (req, res) => {
-    const { name, email, phone, password } = req.body;
-    if (!email || !name || !password ||!phone) {
-        res.status(422).json({ error: "Some arguments are missing" });
+    const { username, password, email, city_id } = req.body;
+    if (!username || !password || !email || !city_id) {
+        return res.status(422).json({ error: "Some arguments are missing" });
     }
-    res.send("Poggers, signed up")
+    con.query(`INSERT INTO user VALUES (DEFAULT, '${username}', '${password}', '${email}', 0, ${city_id}, 0)`, function (err, result) {
+        if (err) {
+            if(err.code == 'ER_DUP_ENTRY') {
+                return res.status(409).json({ error: "Email or username already exists"});
+            }
+        }
+        else {
+            console.log(result);
+            return res.send(
+                jwt.sign(
+                    { user_id: result.insertId },
+                    process.env.JWT_SECRET,
+                    {expiresIn: jwt_expiry_time}
+                )
+            );
+        }
+    });
 })
 
 //logs in a user, requires {email, password}
 router.post('/login', (req, res) => {
-    const {email, password} = req.body
-    if(!email||!password){
+    const {username, password} = req.body
+    if(!username||!password){
         return res.status(422).json({error:"Email or password is missing"});
     }
-
-    res.send("Logged in, Pog")
+    con.query(`SELECT user_id, password FROM user WHERE username = '${username}'`, function (err, result) {
+        console.log(result);
+        if (err) throw err;
+        if(result.length && result[0].password == password)
+            return res.send(
+                jwt.sign(
+                    { user_id: result[0].user_id },
+                    process.env.JWT_SECRET,
+                    {expiresIn: jwt_expiry_time}
+                )
+            );
+        else
+            return res.send("Wrong credentials");
+    });
 })
 
 //gets user data
 router.get('/userData', requireLogin, (req, res) => {
     const {authorization} = req.headers;
-    const id = jwt.decode(authorization.replace("Bearer ",""))
-    
-    res.send("User data, Pog")
+    const payload = jwt.decode(authorization.replace("Bearer ",""))
+
+    con.query(`SELECT username, email, city.city_name, city.city_id, score
+                FROM user
+                INNER JOIN city ON user.city_id = city.city_id
+                WHERE user_id = ${payload.user_id}`,
+        function (err, result) {
+            console.log(result);
+            if (err) throw err;
+            if(result.length)
+                return res.send(result[0]);
+        }
+    );
 })
 
 module.exports = router;
